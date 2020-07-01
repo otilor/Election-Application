ef4d091d7b10585db9e528a41530243c46b46c03
GabielFemi <gabrielfemi799@gmail.com>
2020-07-01 01:52:55 +0100

    Add new table 'polls' to the database

---
+++ database/migrations/2020_07_01_004821_create_polls_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('polls', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('position_id', false);
+            $table->unsignedBigInteger('session_id', false);
+            $table->timestamps();
+
+            $table->foreign('position_id')->references('id')->on('positions');
+            $table->foreign('session_id')->references('id')->on('sessions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('polls');
+    }
+}
