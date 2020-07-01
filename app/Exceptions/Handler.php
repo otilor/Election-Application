3ceb3eeec25754b234e3fe066b44d35bdf5acbea
GabielFemi <gabrielfemi799@gmail.com>
2020-07-01 12:41:19 +0100

    Return a json response with 404 status code if a particular resource is not found

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -50,6 +51,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'error' => 'Resource not found!'
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
