d2c10c8e7c503f8ccf72a125944105505b78e0ed
GabielFemi <gabrielfemi799@gmail.com>
2020-07-01 01:06:53 +0100

    Change title and links in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mapoly SUG Election API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a target = "_blank" href="https://github.com/GabielFemi">Gabriel Akinyosoye</a>
+
                 </div>
             </div>
         </div>
