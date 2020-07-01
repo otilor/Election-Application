379752471953882d2236f731bf88f772b19f4c06
GabielFemi <gabrielfemi799@gmail.com>
2020-07-01 08:06:06 +0100

    Add DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => Str::random(10),
+                'email'=>Str::random(10).'@gmail.com',
+                'password' => Hash::make('password'),
+            ]);
+        }
     }
 }
