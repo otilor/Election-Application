7ad5fd1a275eb5da613e70baa4f8043bce4d5338
GabielFemi <gabrielfemi799@gmail.com>
2020-07-01 07:36:51 +0100

    Start building API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+    $data = ['message' => 'welcome', 'status' => '200'];
+   return response()->json($data, 200);
+});
