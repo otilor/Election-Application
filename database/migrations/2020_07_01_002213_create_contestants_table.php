f64d7aa3a2006d787d0577d50970be8354343ab8
GabielFemi <gabrielfemi799@gmail.com>
2020-07-01 01:30:45 +0100

    Finish migration for contestants

--- database/migrations/2020_07_01_002213_create_contestants_table.php
+++ database/migrations/2020_07_01_002213_create_contestants_table.php
@@ -15,7 +15,12 @@ class CreateContestantsTable extends Migration
     {
         Schema::create('contestants', function (Blueprint $table) {
             $table->id();
+            $table->string('matric_number');
+            $table->unsignedBigInteger('poll_id', false);
+            $table->string('winner')->default('no');
             $table->timestamps();
+
+            $table->foreign('poll_id')->references('id')->on('users');
         });
     }
 
