321745c0c233b9bdf9f6f6ac383d55cff47f46bb
GabielFemi <gabrielfemi799@gmail.com>
2020-07-03 19:51:27 +0100

    Add new font: Calibre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-      <title>Mapoly Student Union Government | Election application</title>
-      <meta name = "csrf_token" content="{{ csrf_token() }}">
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-       <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-       <link rel = "stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
-       @include('includes.navbar')
-    </head>
-    <body id = "home">
+@extends('layouts.app')
 
-           <div id = "app" class="container">
-              <welcome></welcome>
-           </div>
-
-           <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
-    </body>
-</html>
+@section('content')
+  <welcome></welcome>
+@endsection
