41a63902fa6cb5a0f0c0a45376632d275066a880
GabielFemi <gabrielfemi799@gmail.com>
2020-07-03 15:07:07 +0100

    Protect application from cross-site request forgery

--- resources/views/polls/index.blade.php
+++ resources/views/polls/index.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name = "csrf_token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
       <title>All the polls go here!</title>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+      <title>Mapoly Student Union Government | Election application</title>
+      <meta name = "csrf_token" content="{{ csrf_token() }}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
