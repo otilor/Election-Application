141cd163bc9e9de86553fe126522ba36ddd2fe80
GabielFemi <gabrielfemi799@gmail.com>
2020-07-03 22:44:37 +0100

    Add style to dummy text

--- resources/views/includes/story.blade.php
+++ resources/views/includes/story.blade.php
@@ -1,4 +1,4 @@
 <section class="story">
 
-	<h2 class="no-margin"><span class = "bg-skorange rounded-4px px-5px tracking-1h-px font-semibold text-18px py-5px mr-8px">NEW!</span><span class="text-18px font-semibold tracking-1h-px">STRIDEKICK HAS NEW LOOK!</span>Create a new account try the updated experience</h2>
+	<h2 class="no-margin"><span class = "bg-skorange rounded-4px px-5px tracking-1h-px font-semibold text-18px py-5px mr-8px">NEW!</span><span class="text-18px font-semibold tracking-1h-px">STRIDEKICK HAS NEW LOOK! </span><span class="text-18px">Create a new account try the updated experience</span></h2>
 </section>
