8239ea2bb21cf4b2278db8af88c9c8010b501f62
GabielFemi <gabrielfemi799@gmail.com>
2020-07-03 19:45:44 +0100

    Add section for polls

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name = "csrf_token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+       <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
+       @include('includes.navbar')
+       
+    </head>
+    <body id = "about">
+
+           <div id = "app" class="container">
+          	@yield('content')
+              
+           </div>
+
+           <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
--- resources/views/polls/index.blade.php
+++ resources/views/polls/index.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name = "csrf_token" content="{{ csrf_token() }}">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-       <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-      <title>All the polls go here!</title>
-    </head>
-    <body id = "about">
+@extends('layouts.app')
 
-           <div id = "app" class="container">
-          
-              <polls></polls>
-           </div>
+@section ('content')
+  <polls></polls>
 
-           <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
-    </body>
-</html>
+@endsection
