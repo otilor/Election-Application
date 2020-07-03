50ada71b77ea7ce5bb75c27823630dcc45766d53
GabielFemi <gabrielfemi799@gmail.com>
2020-07-03 13:05:41 +0100

    Add HomeController to manage home route

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', 'SpaController@index')->where('any', '.*');
+// Route::get('/{any}', 'SpaController@index')->where('any', '.*');
+Route::get('/', 'HomeController@index');
