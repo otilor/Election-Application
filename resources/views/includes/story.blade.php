20897c46758840417cfd1b5f9f5061b5168fc44b
GabielFemi <gabrielfemi799@gmail.com>
2020-07-03 22:21:41 +0100

    Finish styling new badge

--- resources/views/includes/story.blade.php
+++ resources/views/includes/story.blade.php
@@ -1,4 +1,4 @@
 <section>
-	
-	<h2 class="story">NEW</h2>
+
+	<h2 class="story"><span class = "bg-skorange rounded-4px px-5px tracking-1h-px font-semibold text-18px py-5px">NEW</span></h2>
 </section>
