36b4a93f3bb3c24716d3aa4942280539301c0121
GabielFemi <gabrielfemi799@gmail.com>
2020-07-03 13:20:24 +0100

    Add route handler for /polls

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('/{any}', 'SpaController@index')->where('any', '.*');
 Route::get('/', 'HomeController@index');
+Route::get('/polls', function () {
+	return "Hey dude @Gabriel is working on this";
+});
