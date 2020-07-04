8dd8a43e77c325a44e0ace1f3e1df63fdead482d
GabielFemi <gabrielfemi799@gmail.com>
2020-07-04 09:24:20 +0100

    Finish styling headline text

--- resources/views/includes/gist.blade.php
+++ resources/views/includes/gist.blade.php
@@ -1,3 +1,3 @@
-<div class="bg-skorange gist">
-	I am just a gist
+<div class="text-white bg-skorange">
+	<h1 class="text-headline text-center mx-auto px-5 pt-8 max-w-780px">Connect a fitness tracker and compete in free step challenges on Stridekick</h1>
 </div>
