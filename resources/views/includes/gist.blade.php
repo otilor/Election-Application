9f4731798617cc3e680436c3bcf5bd8895657bc0
GabielFemi <gabrielfemi799@gmail.com>
2020-07-04 09:45:23 +0100

    Add styling to link in text body

--- resources/views/includes/gist.blade.php
+++ resources/views/includes/gist.blade.php
@@ -1,5 +1,7 @@
 <div class="text-white bg-skorange">
 	<h1 class="text-headline text-center mx-auto px-5 pt-8 max-w-780px">Connect a fitness tracker and compete in free step challenges on Stridekick</h1>
 
-	<h2 class="lg:mt-15px text-center text-white text-body md:max-w-620px md:mx-auto px-5">Play for free with friends and family. Or, run a step challenge for your organization on</h2>
+	<h2 class="lg:mt-15px text-center text-white text-body md:max-w-620px md:mx-auto px-5">Play for free with friends and family. Or, run a step challenge for your organization on <a href = "https://github.com/GabielFemi" style="border-width: 1px; border-color: #fff;">MoveSpring</a></h2>
+
+
 </div>
