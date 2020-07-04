a91502fa67eb231e6796a9ba9f429f8fef54e488
GabielFemi <gabrielfemi799@gmail.com>
2020-07-04 13:25:32 +0100

    Add sub text

--- resources/views/includes/gist.blade.php
+++ resources/views/includes/gist.blade.php
@@ -2,6 +2,14 @@
 	<h1 class="text-headline text-center mx-auto px-5 pt-8 max-w-780px">Signup and vote in the executive of your choice in a fantastic and awesome way</h1>
 
 	<h2 class="lg:mt-15px text-center text-white text-body md:max-w-620px md:mx-auto px-5">Play for free with friends and family. Or, run a step challenge for your organization on <a href = "https://github.com/GabielFemi" style="border-width: 1px; border-color: #fff;">MoveSpring</a></h2>
+	
 
+	 <div class="lg:flex lg:mx-auto lg:max-w-1024px">
+		<div class="mx-20px mt-25px lg:mt-50px pt-25px px-25px lg:pt-35 lg:px-35px bg-white">
+			<h2 class="text-sub text-dark mt-16px">Compete with family and friends</h2>
+		</div>
+	</div>
 
 </div>
+
+ 
