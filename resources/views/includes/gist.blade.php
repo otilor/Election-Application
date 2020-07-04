f27739f49d8769074b5d3be19a30eadffcf08f09
GabielFemi <gabrielfemi799@gmail.com>
2020-07-04 09:35:16 +0100

    Add body for headline with styles

--- resources/views/includes/gist.blade.php
+++ resources/views/includes/gist.blade.php
@@ -1,3 +1,5 @@
 <div class="text-white bg-skorange">
 	<h1 class="text-headline text-center mx-auto px-5 pt-8 max-w-780px">Connect a fitness tracker and compete in free step challenges on Stridekick</h1>
+
+	<h2 class="lg:mt-15px text-center text-white text-body md:max-w-620px md:mx-auto">Play for free with friends and family. Or, run a step challenge for your organization on MoveSpring</h2>
 </div>
