9df52ee2f0078e33f727474a3a458a82014d8840
GabielFemi <gabrielfemi799@gmail.com>
2020-07-05 17:15:11 +0100

    Add style for end of story description

--- resources/views/includes/gist.blade.php
+++ resources/views/includes/gist.blade.php
@@ -36,7 +36,7 @@
 	<div class="mx-auto my-5 col-md-6">
 		<div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-mpblue">
 			<h2 class="text-headline text-white">Enough of the story right?</h2>
-			<h4 class="text-body">Let's jump right into it</h4>
+			<h4 class="text-body text-mpblue-500">Let's jump right into it</h4>
 		</div>
 	</div>
 		
