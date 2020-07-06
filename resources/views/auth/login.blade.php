5d9d3df0c39718465c4a379cb2e672884ffd954b
GabielFemi <gabrielfemi799@gmail.com>
2020-07-06 12:09:35 +0100

    Add style to login text

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="card my-1 bg-white">
+                <div class="card-header text-body">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
