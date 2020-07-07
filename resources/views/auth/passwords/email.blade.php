16e547ca467f035fdb02489c15bbd527e36a4f25
GabielFemi <gabrielfemi799@gmail.com>
2020-07-07 12:40:45 +0100

    Add style to card header and card body in password reset page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,9 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header text-header text-body text-caps">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
+                <div class="card-body text-semibold">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
