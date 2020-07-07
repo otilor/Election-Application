91b48b842da289a4f41703abdf74bd76023d52a7
GabielFemi <gabrielfemi799@gmail.com>
2020-07-07 16:07:19 +0100

    Show home pane when the user is not authenticated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+@guest
 @include('includes.gist')
   {{-- <welcome></welcome> --}}
+
+ @endguest
+
+ @auth
+ <p>Sorry, but I need to add something here!</p>
+ @endauth
 @endsection
