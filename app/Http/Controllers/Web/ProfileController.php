171cf81802fe19a3d186065dba83ff7cd34fb7bd
GabielFemi <gabrielfemi799@gmail.com>
2020-07-07 20:41:24 +0100

    Add authentication middleware

--- app/Http/Controllers/Web/ProfileController.php
+++ app/Http/Controllers/Web/ProfileController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    public function __construct() 
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
