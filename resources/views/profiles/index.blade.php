5082235b0713b29d8ad06ad89ee6bddd73899168
GabielFemi <gabrielfemi799@gmail.com>
2020-07-07 16:04:31 +0100

    Add alert div to alert pane with message

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -3,21 +3,22 @@
 
 @section('content')
 <section class="random-div bg-not-too-white text-mpgray">
+<div class="mx-auto text-center text-semibold">
+	<div class="alert alert-danger">For now, you cannot edit your profile!</div>	
+</div>
+
 	<h1 class="text-center py-15px text-35px text-semibold">Your profile</h1>
-	<h3 class="text-23px mx-auto text-random-light random-div-title-description text-center">NAME</h3>
+	
 	<div class="text-center">
 		<img class = "rounded-img img-responsive voting-icon" src="{{ asset('img/male.webp') }}">
 	</div>
+	<h3 class="text-23px p-5 text-mpgray mx-auto random-div-title-description text-center">{{ Auth::user()->name }}</h3>
+
+
 	
-	<div class="mx-auto my-5 col-md-6">
-		<div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-mpblue">
-			<h2 class="text-headline text-white">Enough of the story right?</h2>
-			<h4 class="text-body text-mpblue-500">Let's jump right into it</h4>
-			<div class="end-of-story-button mt-20px">
-				<a href="/polls" class="py-3 br-10px px-4 bg-msblue-600  text-caps">start off</a>
-			</div>
-		</div>
-	</div>
+
+
+
 		
 	
 	
