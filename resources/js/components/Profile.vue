34f10e12d29ecef20081123ed48637844df4dcb7
GabielFemi <gabrielfemi799@gmail.com>
2020-07-08 03:31:24 +0100

    Add new component - Profile

---
+++ resources/js/components/Profile.vue
@@ -0,0 +1,28 @@
+<template>
+	<p>I am a profile component	</p>
+
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				users: [],
+				user: {
+					name: '',
+					email: '',
+				},
+
+			}
+			
+		},
+
+		methods: {
+			getAuthenticatedUser: function () {
+				fetch('api/user').then(res => res.json()).then(res => {
+					this.users = res.data;
+				})
+			}
+		}
+	}
+</script>
