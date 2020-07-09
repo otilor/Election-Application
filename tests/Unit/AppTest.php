9304015dbaf5c1b4c888a21f9d284ebe65db2a49
GabielFemi <gabrielfemi799@gmail.com>
2020-07-09 14:27:01 +0100

    Add tests

---
+++ tests/Unit/AppTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertFalse(false);
+    }
+}
