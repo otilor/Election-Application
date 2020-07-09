cc6d15b4a6722b806e3df3a4afde825ee6b64d30
GabielFemi <gabrielfemi799@gmail.com>
2020-07-09 15:12:21 +0100

    Change name of input field

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -18,7 +18,7 @@
 
 		<form action = "/profile/update" method = "post">
 			@csrf
-			<input class = "p-1 text-semibold rounded-30px" type="text" value="{{ Auth::user()->name }}">
+			<input class = "p-1 text-semibold rounded-30px"  name = "name" type="text" value="{{ Auth::user()->name }}">
 			<input class="btn text-white text-caps btn-success edit" type = "submit" value = "Save">
 		</form>	
 	</div>
