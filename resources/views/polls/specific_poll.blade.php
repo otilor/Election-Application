142d5405d19d6c49de20197ce3cd31d1c0c66ccc
GabielFemi <gabrielfemi799@gmail.com>
2020-07-11 12:19:42 +0100

    Consume $position details in the view

--- resources/views/polls/specific_poll.blade.php
+++ resources/views/polls/specific_poll.blade.php
@@ -2,6 +2,15 @@
 
 
 @section('content')
-<p>{{ json_encode($poll) }}</p>
-<p>{{ json_encode($positions) }}</p>
+
+
+@forelse ($positions as $position)
+				
+				<div class="card m-3">
+					<div class="card-header text-skgray text-caps text-semibold">{{ $position->title }}</div>
+					<div class="card-body">{{ $position->description }}</div>
+				</div>
+    @empty
+		<h1 class="text-center text-danger text-headline">No polls!</h1>
+  @endforelse
 @endsection
