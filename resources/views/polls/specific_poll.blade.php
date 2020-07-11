39ed71f498b933461e9ffbb93b9fd022b7b5bf43
GabielFemi <gabrielfemi799@gmail.com>
2020-07-11 14:03:27 +0100

    Add style to all the positions in the poll

--- resources/views/polls/specific_poll.blade.php
+++ resources/views/polls/specific_poll.blade.php
@@ -6,10 +6,20 @@
 
 @forelse ($positions as $position)
 				
-				<div class="card m-3">
-					<div class="card-header text-skgray text-caps text-semibold">{{ $position->title }}</div>
-					<div class="card-body">{{ $position->description }}</div>
+			
+<div class="container">
+        <div class="row justify-content-center">
+		     <div class="mx-auto my-3 col-md-6">
+				<div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-skorange">
+					<h2 class="text-headline text-white">{{ $position->title }}</h2>
+
+					<div class="end-of-story-button mt-20px">
+						<a href = "/" class="py-3 br-10px text-dark px-4 bg-white  text-caps">Continue!</a>
+					</div>
 				</div>
+			</div>
+        </div>
+    </div>
     @empty
 		<h1 class="text-center text-danger text-headline">No polls!</h1>
   @endforelse
