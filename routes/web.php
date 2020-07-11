f24bdc0d6f99ace137ccdb9cdb5e936a15f6ed00
GabielFemi <gabrielfemi799@gmail.com>
2020-07-11 15:01:11 +0100

    Create new resource route /positions

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/polls', 'Web\PollController')->middleware('auth');
+
+Rotue::resource('/positions', 'Web\PostionController')->middleware('auth');
