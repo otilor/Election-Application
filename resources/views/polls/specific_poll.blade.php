bb6345acebe53b5e97ebdde95d5e1d21ee10366a
GabielFemi <gabrielfemi799@gmail.com>
2020-07-11 14:14:25 +0100

    Add header text with style definitions

--- resources/views/polls/specific_poll.blade.php
+++ resources/views/polls/specific_poll.blade.php
@@ -3,11 +3,12 @@
 
 @section('content')
 
-
+<p class="bg-deepblue text-center py-3 text-caps text-white">There are currently {{ count($positions) }} open positions for this poll</p>
 @forelse ($positions as $position)
 				
 			
-<div class="container">
+
+
         <div class="row justify-content-center">
 		     <div class="mx-auto my-3 col-md-6">
 				<div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-skorange">
@@ -19,8 +20,8 @@
 				</div>
 			</div>
         </div>
-    </div>
+
     @empty
-		<h1 class="text-center text-danger text-headline">No polls!</h1>
+		<h1 class="text-center text-danger text-headline">No vacant positions!</h1>
   @endforelse
 @endsection
