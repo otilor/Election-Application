a6936dc526582cecc6caf5cc752e3a4079fef616
GabielFemi <gabrielfemi799@gmail.com>
2020-07-12 04:31:48 +0100

    Remove column 'position_id' from 'polls' table

--- database/migrations/2020_07_01_001900_create_polls_table.php
+++ database/migrations/2020_07_01_001900_create_polls_table.php
@@ -18,12 +18,10 @@ class CreatePollsTable extends Migration
             $table->string('title');
             $table->mediumText('description');
             $table->unsignedBigInteger('link_id', false);
-            $table->unsignedBigInteger('position_id', false);
             $table->unsignedBigInteger('session_id', false);
             $table->timestamps();
 
             $table->foreign('link_id')->references('id')->on('links')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('position_id')->references('id')->on('positions')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('session_id')->references('id')->on('sessions')->onUpdate('cascade')->onDelete('cascade');
         });
     }
