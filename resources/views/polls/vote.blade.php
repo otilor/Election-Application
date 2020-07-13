62c0f6ed9e98560bafd9209c65a675315f9eb757
GabielFemi <gabrielfemi799@gmail.com>
2020-07-13 03:36:46 +0100

    Get all the contestants attached to a certain position and show them on the 'polls.vote' page

--- resources/views/polls/vote.blade.php
+++ resources/views/polls/vote.blade.php
@@ -1,1 +1,13 @@
-<p>So you want to vote for right?</p>
+@extends ('layouts.app')
+
+
+@section ('content')
+<p class = "text-semibold pt-3 text-center text-headline">Vote</p>
+<div class = "text-semibold mx-3 alert alert-info">{{ count($all_details["contestants"]) }} contestant vying for this position</div>
+
+{{-- List all the section --}}
+<section class = "mx-3">
+	<p>{{ json_encode($all_details["contestants"]) }}</p>	
+</section>
+
+@endsection
