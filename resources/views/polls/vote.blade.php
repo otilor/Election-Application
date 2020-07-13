975a85b1248f5701212c3337c3a7042bfde0f325
GabielFemi <gabrielfemi799@gmail.com>
2020-07-13 04:18:01 +0100

    Refactor code: add control structure to 'polls.vote'

--- resources/views/polls/vote.blade.php
+++ resources/views/polls/vote.blade.php
@@ -2,14 +2,16 @@
 
 
 @section ('content')
-<p class = "text-semibold pt-3 text-center text-headline">Vote</p>
-<div class = "text-semibold mx-3 alert alert-info">{{ count($all_details["contestants"]) }} contestant vying for this position</div>
+
+
 <section class="story">
 	<a href="/polls">
-		<h2 class="no-margin"><span class = "{{ empty (count($all_details["contestants"])) ? 'bg-danger':'bg-success' }} rounded-4px px-5px tracking-1h-px font-semibold text-18px py-5px mr-8px">{{ count($all_details["contestants"]) }}</span><span class="text-18px font-semibold tracking-1h-px">Contestants! </span><span class="text-18px">Check other polls</span></h2>	
+		<h2 class="no-margin"><span class = "{{ empty (count($all_details["contestants"])) ? 'bg-danger':'bg-success' }} rounded-4px px-5px tracking-1h-px font-semibold text-18px py-5px mr-8px">{{ count($all_details["contestants"]) }}</span><span class="text-18px font-semibold tracking-1h-px">Contestant(s)! </span><span class="text-18px">{{ empty (count($all_details["contestants"])) ? 'Check other polls' : '' }}</span></h2>	
 	</a>
 	
 </section>
+
+
 {{-- List all the section --}}
 <section class = "mx-3">
 	<p>{{ json_encode($all_details["contestants"]) }}</p>	
