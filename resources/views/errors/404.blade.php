1c9f097703255ddb8d9bd4d1a7afe76fc54f7892
GabielFemi <gabrielfemi799@gmail.com>
2020-07-13 14:34:08 +0100

    Finish handling 404 Exception

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,7 @@
 @extends ('layouts.app')
 
 @section ('content')
-
+<div class="text-center">
+		<img class = "rounded-img img-responsive voting-icon" src="{{ asset('img/404.png') }}">
+</div>
 @endsection
