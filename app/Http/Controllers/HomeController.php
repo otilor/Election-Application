3534d5e0d685871f2c041e405cb240a04d3d8599
GabielFemi <gabrielfemi799@gmail.com>
2020-07-13 14:17:19 +0100

    Add middleware

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+	// Only guests can view this.
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
+
     //
     public function index() {
     	return view('welcome');
