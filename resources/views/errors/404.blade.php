8f4af7f30d001913f64fca766be97600909dd0e3
GabielFemi <gabrielfemi799@gmail.com>
2020-07-13 14:41:35 +0100

    Add action button to 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,5 +3,6 @@
 @section ('content')
 <div class="text-center">
 		<p class="p-5 text-headline text-danger">Page not found!</p>
+		<a href="/" class="no-gutters text-white uppercase tracking-2px border-2 border-skorange py-top-10px signup rounded-10px text-20px mr-sm-2" type="submit">Go home</a>
 </div>
 @endsection
