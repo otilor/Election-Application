9e9fac64eab5ec37bdcf52900d0313dd8b42f121
GabielFemi <gabrielfemi799@gmail.com>
2020-07-16 13:22:22 +0100

    Initialize new class with method fetchDetailsUsingPositon(@param)

---
+++ Helpers/Details/AllDetails.php
@@ -0,0 +1,14 @@
+<?php 
+
+/*
+* Contains all the details needed by the users
+* @see PositionDetials
+*/
+
+class AllDetails implements Interfaces\DetailsInterface
+{
+	public function fetchDetailsUsingPosition($id)
+	{
+		// 
+	}
+}
