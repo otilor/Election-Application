9943488eb7424798557a19106163c1d6bed5cd1c
GabielFemi <gabrielfemi799@gmail.com>
2020-07-16 13:31:35 +0100

    Remove contract 'details'

--- Interfaces/DetailsInterface.php
+++ Interfaces/DetailsInterface.php
@@ -4,8 +4,6 @@ namespace Interfaces;
 
 interface DetailsInterface
 {
-	// Default details method
-	private function details();
 
 	// If there is a specific identifier, proceed to use to get resource
 	public function detailsFromId($id);
