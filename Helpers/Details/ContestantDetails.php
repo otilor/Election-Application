2a447f41667bbc6a4e44d8f333a75e03d6c0e779
GabielFemi <gabrielfemi799@gmail.com>
2020-07-16 02:44:03 +0100

    Add new classes to contain actions for Contestant and Poll details

---
+++ Helpers/Details/ContestantDetails.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Helpers\Details;
+use Interfaces\DetailsInterface;
+
+class ContestantDetails implements DetailsInterface
+{
+	public function output ()
+	{
+		dd ("I am in contestant details!");
+	}
+}
---
+++ Helpers/Details/PollDetails.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Helpers\Details;
+
+class PollDetails 
+{
+	//
+}
