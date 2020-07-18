ced8cac962aa9d684b3f885a45c75c37643894d8
GabielFemi <gabrielfemi799@gmail.com>
2020-07-18 13:46:37 +0100

    Refactor code: remove blade validation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-@guest
-@include('includes.gist')
-  {{-- <welcome></welcome> --}}
 
- @endguest
 
- @auth
+ 
  <div class="text-center py-5">
         
         <img class = "py-3" style = "width: 50vh;" src="{{ asset('img/caution.png') }}">
         <h1>Work is currently in progress!</h1>
         <h3 class="text-caps">The craftsmen are toiling day and night to make this happen. Trust me!</h3>
 </div>
- @endauth
+
 @endsection
