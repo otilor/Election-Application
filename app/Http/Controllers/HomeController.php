efcef4df2fb9d8f39425c0cfc41aa948cc50d515
GabielFemi <gabrielfemi799@gmail.com>
2020-07-19 17:14:28 +0100

    Create new middleware for student roles
    Signed-off-by: GabielFemi <gabrielfemi799@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,6 @@ class HomeController extends Controller
     public function index() {
     	if ( auth()->check() )
     	{
-    		dump ( auth()->user()->permissions );
     		return view('home');
     	} 
     	else 
---
+++ app/Http/Middleware/Student.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Student
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (! auth()->check() )
+        {
+            return redirect()->route('login');
+        }
+        $request->user;
+        return $next($request);
+    }
+}
