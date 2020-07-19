607da0c075bf9b220b6bc401df7b7d6c2507cd3e
GabielFemi <gabrielfemi799@gmail.com>
2020-07-19 19:57:04 +0100

    Comment API exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,11 +51,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        if ($exception instanceof ModelNotFoundException) {
-            return response()->json([
-                'error' => 'Resource not found!'
-            ], 404);
-        }
+        // if ($exception instanceof ModelNotFoundException) {
+        //     return response()->json([
+        //         'error' => 'Resource not found!'
+        //     ], 404);
+        // }
 
         if ($this->isHttpException($exception)) {
             if ($exception->getStatusCode() == 404) {
