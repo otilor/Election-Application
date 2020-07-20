449f30b5743bbd50843895e98fa2c3003b8ce7f2
GabielFemi <gabrielfemi799@gmail.com>
2020-07-20 11:11:57 +0100

    Add new routes to student routes

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 
-Route::resource('/positions', 'Web\PositionController');
-
-Route::post('vote', 'Web\VoteController@store');
-
 // Student routes
 Route::group(['prefix' => 'student'], function () {
 	Route::resource('/polls', 'Web\PollController');
+	Route::resource('/positions', 'Web\PositionController');
+	Route::post('vote', 'Web\VoteController@store');
 });
