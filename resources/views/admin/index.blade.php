5a2253fa79c477c746b2c03f72b4a864efda3d1f
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-21 17:19:42 +0100

    Refactor code: change text

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,7 @@
         <div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-mpblue">
           <h2 class="text-headline text-white">Student dashboard</h2>
           <div class="end-of-story-button mt-20px">
-            <a href = "polls/3 " class="py-3 br-10px px-4 bg-msblue-600  text-caps">Check out!</a>
+            <a href = "admin" class="py-3 br-10px px-4 bg-msblue-600  text-caps">Check out!</a>
           </div>
         </div>
       </div>
@@ -16,9 +16,9 @@
         <div class="row justify-content-center">
          <div class="mx-auto my-3 col-md-6">
         <div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-success">
-          <h2 class="text-headline text-white">Admin dashboard</h2>
+          <h2 class="text-headline text-white">Election dashboard</h2>
           <div class="end-of-story-button mt-20px">
-            <a href = "polls/3 " class="py-3 br-10px px-4 bg-light text-dark text-caps">Check out!</a>
+            <a href = "#" class="py-3 br-10px px-4 bg-light text-dark text-caps">Check out!</a>
           </div>
         </div>
       </div>
