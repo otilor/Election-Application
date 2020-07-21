13c876c5e2d0ebf9f1a637d2f75447f6eda2dfdf
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-21 17:46:44 +0100

    Refactor code: add create card and view cards

--- resources/views/admin/student/index.blade.php
+++ resources/views/admin/student/index.blade.php
@@ -1,6 +1,30 @@
 @extends ('layouts.app')
 @section ('content')
-<h3 class="text-center text-semibold text-caps p-3">Current users</h3>
+<div class="container">
+        <div class="row justify-content-center">
+         <div class="mx-auto my-3 col-md-6">
+        <div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-mpblue">
+          <h2 class="text-headline text-white">View all accounts</h2>
+          <div class="end-of-story-button mt-20px">
+            <a href = "/admin/student" class="py-3 br-10px px-4 bg-msblue-600  text-caps">View -></a>
+          </div>
+        </div>
+      </div>
+        </div>
+    </div>
+<div class="container">
+        <div class="row justify-content-center">
+         <div class="mx-auto my-3 col-md-6">
+        <div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-success">
+          <h2 class="text-headline text-white">Create an account</h2>
+          <div class="end-of-story-button mt-20px">
+            <a href = "/" class="py-3 br-10px px-4 bg-light text-success text-caps">Create</a>
+          </div>
+        </div>
+      </div>
+        </div>
+    </div>
+<!-- <h3 class="text-center text-semibold text-caps p-3">Current users</h3>
 <table class="table">
   <thead class="thead-dark">
     <tr>
@@ -18,5 +42,5 @@
     </tr>
   @endfor
   </tbody>
-</table>
+</table> -->
 @endsection
