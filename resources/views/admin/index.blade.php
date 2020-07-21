ae13c423c33890929558064f67bdf09411845795
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-21 17:14:48 +0100

    Add new cards in index page

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,28 @@
+@extends ('layouts.app')
+@section ('content')
+<div class="container">
+        <div class="row justify-content-center">
+         <div class="mx-auto my-3 col-md-6">
+        <div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-mpblue">
+          <h2 class="text-headline text-white">Student dashboard</h2>
+          <div class="end-of-story-button mt-20px">
+            <a href = "polls/3 " class="py-3 br-10px px-4 bg-msblue-600  text-caps">Continue!</a>
+          </div>
+        </div>
+      </div>
+        </div>
+    </div>
+<div class="container">
+        <div class="row justify-content-center">
+         <div class="mx-auto my-3 col-md-6">
+        <div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-success">
+          <h2 class="text-headline text-white">Admin dashboard</h2>
+          <div class="end-of-story-button mt-20px">
+            <a href = "polls/3 " class="py-3 br-10px px-4 bg-light text-dark text-caps">Elect!</a>
+          </div>
+        </div>
+      </div>
+        </div>
+    </div>
+
+    @endsection
