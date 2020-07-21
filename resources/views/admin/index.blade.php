f594f57e0861d291c2cdbd1455132226b6bc633e
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-21 17:35:01 +0100

    Refactor code: change route

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,7 @@
         <div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-mpblue">
           <h2 class="text-headline text-white">Student dashboard</h2>
           <div class="end-of-story-button mt-20px">
-            <a href = "admin" class="py-3 br-10px px-4 bg-msblue-600  text-caps">Check out!</a>
+            <a href = "/admin/student" class="py-3 br-10px px-4 bg-msblue-600  text-caps">Check out!</a>
           </div>
         </div>
       </div>
@@ -18,7 +18,7 @@
         <div class="flex-grow story-ended p-5 br-20px m-1 col-md-12 bg-success">
           <h2 class="text-headline text-white">Election dashboard</h2>
           <div class="end-of-story-button mt-20px">
-            <a href = "#" class="py-3 br-10px px-4 bg-light text-dark text-caps">Check out!</a>
+            <a href = "/" class="py-3 br-10px px-4 bg-light text-dark text-caps">Check out!</a>
           </div>
         </div>
       </div>
