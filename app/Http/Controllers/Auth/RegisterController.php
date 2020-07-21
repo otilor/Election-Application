bcef06e8e166bcfdf8e2933850d9e76788419a52
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-21 10:21:05 +0100

    Assign role of student upon registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,10 +64,11 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        return $user->assignRole('student');
     }
 }
