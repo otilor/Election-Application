e354bd8a894baa45dd757d6f9e44097f35f9317a
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-21 12:42:42 +0100

    Add method for getting users for administrators

--- app/Http/Controllers/Web/Admin/AdminController.php
+++ app/Http/Controllers/Web/Admin/AdminController.php
@@ -14,6 +14,7 @@ class AdminController extends Controller
      */
     public function index()
     {
+        return \App\User::allUsersForAdministrators();
         return view ('admin.index');
     }
 
