2ed9b496c8b67a6bb060dfdb9f0621359ca3e031
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-22 06:52:48 +0100

    Handle creation of students: add route declaration

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,5 @@ Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
 	Route::get('/', 'Web\Admin\AdminController@index');
 	Route::get('/students', 'Web\Admin\StudentController@index');	
 	Route::get('students/accounts', 'Web\Admin\StudentController@accounts');
+	Route::get('students/create', 'Web\Admin\StudentController@create');
 });
