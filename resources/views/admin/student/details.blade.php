4cd170bb7db44536157daa50d25f3eac35cc776f
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-22 11:04:47 +0100

    Refactor code: made it more readable

--- resources/views/admin/student/details.blade.php
+++ resources/views/admin/student/details.blade.php
@@ -1,6 +1,5 @@
 @extends ('layouts.app')
 
-
 @section('content')
 <section class="random-div bg-not-too-white text-mpgray">
 	<h1 class="text-center py-15px text-35px text-semibold">User profile</h1>
@@ -9,11 +8,14 @@
 		<img class = "rounded-img img-responsive voting-icon" src="{{ asset('img/male.webp') }}">
 	</div>
 	
-	
 	<div>
-		<h3 class="text-23px pt-5 pb-1 text-mpgray mx-auto random-div-title-description text-center">{{ $user->name }}</h3>
+		<h3 class="text-23px pt-5 pb-1 text-mpgray mx-auto random-div-title-description text-center">
+			{{ $user->name }}
+		</h3>
 
-	<h4 class = "text-center">Your email address:  {{ $user->email }} </h4>		
+		<h4 class = "text-center">Your email address:  
+			{{ $user->email }} 
+		</h4>		
 	</div>
 
 	<div class="text-center">
