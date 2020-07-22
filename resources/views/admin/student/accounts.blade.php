c1081d3d29f60468dc25baea0208c04edfdcad5b
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-22 06:34:36 +0100

    Add table to display user details clear

--- resources/views/admin/student/accounts.blade.php
+++ resources/views/admin/student/accounts.blade.php
@@ -3,5 +3,21 @@
 @section ('content')
 <div class="container pt-3">
 	<h4 class="text-center">Accounts</h4>
+
+	<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email address</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+  </tbody>
+</table>
 </div>
 @endsection
