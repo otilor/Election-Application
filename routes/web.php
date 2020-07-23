1e0c5ff35e67e0a32131b07107c03542c6748da8
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-23 15:00:14 +0100

    Use controller method to handle PATCH request

--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,5 @@ Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
 	Route::get('students/create', 'Web\Admin\StudentController@create');
 	Route::post('students/store', 'Web\Admin\StudentController@store')->name('admin.students.store');
 	Route::get('students/{id}', 'Web\Admin\StudentController@show');
-	Route::patch('students/{id}', function ()
-	{
-		return "I am a patch request";
-	});
+	Route::patch('students/{id}', 'Web\Admin\StudentController@update');
 });
