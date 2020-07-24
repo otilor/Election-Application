f416b1bdb6184d9c023741cd18b8c252dd4b09c0
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-24 16:12:01 +0100

    Remove HTML element

--- resources/views/admin/polls-sessions-links/polls/create.blade.php
+++ resources/views/admin/polls-sessions-links/polls/create.blade.php
@@ -1,1 +1,29 @@
-create.blade.php
+@extends ('layouts.app')
+@section ('content')
+<h3 class="text-center pt-4">Create a new poll</h3>
+<div class="container">
+	@include ('includes.messages')
+	<form action = "{{ route('admin.students.store') }}" method="post">
+		@csrf
+		  <div class="form-group">
+		    <label for="exampleFormControlInput1">Title</label>
+		    <input type="text" name = "name" class="form-control" id="exampleFormControlInput1" placeholder="Executive poll">
+		  </div>
+
+		  <div class="form-group">
+		    <label for="exampleFormControlInput1">Description</label>
+		    <textarea class="form-control"></textarea>
+		  </div>
+
+		  <div class="form-group">
+		    <label for="exampleFormControlInput1">Password</label>
+	    	<input type="text" name = "password" class="form-control" id="exampleFormControlInput1">
+		  </div>
+		  
+		  <div class="form-group text-center">
+		  	<input type="submit" class="btn btn-dark" value="Create">
+		  </div>
+	</form>
+</div>
+
+@endsection
