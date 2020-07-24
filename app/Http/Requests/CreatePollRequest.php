40602f72d89546c44f500046e77453e892ae3ad8
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-24 11:24:30 +0100

    Update Request class

--- app/Http/Requests/CreatePollRequest.php
+++ app/Http/Requests/CreatePollRequest.php
@@ -13,6 +13,11 @@ class CreatePollRequest extends FormRequest
      */
     public function authorize()
     {
+        if ( auth()->user()->hasRole('admin') )
+        {
+            return true;
+        }
+        
         return false;
     }
 
