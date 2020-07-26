9825d63fe442874a0d8fb83e6fd7d6793c1be1ac
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-26 20:40:32 +0100

    Verify positions

---
+++ resources/views/admin/positions/create.blade.php
@@ -0,0 +1,32 @@
+@extends ('layouts.app')
+@section ('content')
+<h3 class="text-center pt-4">Add position</h3>
+<div class="container">
+	@include ('includes.messages')
+	<form action = "/admin/polls/store" method="post">
+		@csrf
+		@method ("post")
+		  <div class="form-group">
+		    <label for="exampleFormControlInput1">Title</label>
+		    <input type="text" name = "title" class="form-control" id="exampleFormControlInput1" placeholder="President">
+		  </div>
+
+		  <div class="form-group">
+		    <label for="exampleFormControlInput1">Description</label>
+		    <textarea name = "description" class="form-control"></textarea>
+		  </div>
+
+		  <div class="form-group">
+		    <label for="exampleFormControlInput1">Position id</label>
+		    <input name = "link_id" class  = "form-control" type="text" readonly="true" value = "{{ $pollId }}">
+		  </div>
+		  
+		  <div class="form-group text-center">
+		  	<input type="submit" class="btn btn-dark" value="Create">
+		  </div>
+
+		  
+	</form>
+</div>
+
+@endsection
