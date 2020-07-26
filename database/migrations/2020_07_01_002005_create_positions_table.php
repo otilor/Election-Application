4028b4e5e045b92265df1b4ed2636d80d0f8325d
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-26 17:18:12 +0100

    Change DB schema

---
+++ database/migrations/2020_07_01_002005_create_positions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('poll_id');
+            $table->string('title');
+            $table->mediumText('description');
+            $table->timestamps();
+
+            $table->foreign('poll_id')->references('id')->on('polls');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+}
