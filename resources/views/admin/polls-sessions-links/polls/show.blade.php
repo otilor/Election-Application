7b6d683c4a52f4f8f1b12932f3a5e16fb33e5ce0
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-27 10:42:06 +0100

    Remove redundant section in view

--- resources/views/admin/polls-sessions-links/polls/show.blade.php
+++ resources/views/admin/polls-sessions-links/polls/show.blade.php
@@ -15,28 +15,4 @@
 		</h3>
 		
 	<h4 class = "text-center">Your email address:  {{ $poll->description }} </h4>
-
-	<p class="card-header alert alert-dark" style="font-size: 18px">Positions</p>
-	<div>
-		@forelse ($poll->positions as $position)
-		<h3 class="text-23px pb-1 text-mpgray mx-auto random-div-title-description text-center">
-			Title: {{ $position->title }}
-			<br>
-			Description: {{ $position->description }}
-		</h3>
-		@empty
-		<div class="text-center">
-			<p class="text-center"><code class="text-dark text-23px">No positions, bruv</code></p>
-			<a  href = "/admin/polls-sessions-links/polls/{{ $poll->id }}/positions/create" class="btn btn-dark text-white">Add one</a>	
-		</div>
-		
-		@endforelse
-		@if ( count($poll->positions) > 0 )
-		<div class="text-center">
-			<a href = "/admin/polls-sessions-links/polls/{{ $poll->id }}/positions/create" class="btn btn-dark text-white">Add another</a>		
-		</div>
-		
-		@endif
-		@endunless
-
 	@endsection
