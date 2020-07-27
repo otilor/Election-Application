e75879863452992b3c59ddd84975d0648673f96a
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-27 09:46:25 +0100

    Use unless directive

--- resources/views/admin/polls-sessions-links/polls/show.blade.php
+++ resources/views/admin/polls-sessions-links/polls/show.blade.php
@@ -1,14 +1,19 @@
 @extends('layouts.app')
 @section ('content')
 <section class="random-div bg-not-too-white text-mpgray">
+	@unless ( is_null($poll ))
 	<p class="card-header alert alert-dark" style="font-size: 18px">Poll information</p>
 	<div class="text-center p-2">
 		<img style = "width: 100px; height: 100px; border-radius: 40px;" src="{{ asset('img/voting.png') }}">
 	</div>
+	
 	<div>
+		
+		
 		<h3 class="text-23px pb-1 text-mpgray mx-auto random-div-title-description text-center">
 			Title: {{ $poll->title }}
 		</h3>
+		
 	<h4 class = "text-center">Your email address:  {{ $poll->description }} </h4>
 
 	<p class="card-header alert alert-dark" style="font-size: 18px">Positions</p>
@@ -32,4 +37,6 @@
 		</div>
 		
 		@endif
+		@endunless
+
 	@endsection
