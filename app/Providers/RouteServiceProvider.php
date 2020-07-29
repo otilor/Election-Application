ef9dd54d32cf340e1ade3f1a89cf1ce0c0099536
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-07-29 10:44:04 +0100

    Change default home page

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/student/polls';
 
     /**
      * Define your route model bindings, pattern filters, etc.
