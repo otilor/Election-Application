71141e9b6961a5a780588175324f47721389967c
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-08-03 09:14:27 +0100

    Fix: remove double hashing of password

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,7 @@ class RegisterController extends Controller
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => $data['password'],
         ]);
         return $user->assignRole('student');
     }
